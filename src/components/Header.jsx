5ce0056c8dadae1d268abc4af0c62d93d4ee52a0
SebastianLl28 <llamucasebas@gmail.com>
2023-09-27 10:39:48 -0500

    style: add header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,43 @@
 import { NavLink } from "react-router-dom";
 import Toggle from "./Toggle";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(true);
+
   return (
-    <header className="w-full dark:bg-gray-900">
-      {/* bg-red-200 sm:bg-red-500 */}
-      <div className="container mx-auto py-4 px-4 flex items-center  justify-between flex-wrap ">
-        <div className="flex-grow basis-0">
+    <header
+      className={`w-full dark:bg-gray-900   ${
+        isActive ? "h-screen fixed top-0" : "relative"
+      }`}
+    >
+      <div
+        className={`container mx-auto py-4 px-4 flex items-center  justify-between flex-wrap gap-x-5 ${
+          isActive && "h-full"
+        }`}
+      >
+        <div className="flex-grow lg:basis-0">
           <Link to="/">
             <img
               src="/logo.png"
-              className="h-20 shadow-logo cursor-pointer"
+              className="sm:h-20 h-14 shadow-logo cursor-pointer"
               alt=""
             />
           </Link>
         </div>
-        <nav>
-          <ul className="flex space-x-6 [&>li>a]:text-white">
+        <nav
+          className={`${
+            isActive ? "block order-2 relative w-full h-full" : "hidden"
+          } lg:block lg:order-none`}
+        >
+          <ul
+            className={`flex [&>li>a]:text-white items-center  ${
+              isActive
+                ? "flex-col content-center h-full justify-center space-y-9 text-2xl font-bold"
+                : "space-x-6"
+            }`}
+          >
             <li>
               <NavLink
                 to="/"
@@ -60,9 +80,28 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <div className="flex-grow basis-0 flex justify-end">
+        <div className="lg:flex-grow lg:basis-0 flex justify-end">
           <Toggle />
         </div>
+        <button
+          className="block lg:hidden cursor-pointer"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path
+              d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6Z"
+              fill="currentColor"
+            />
+            <path
+              d="M2 12.0322C2 11.4799 2.44772 11.0322 3 11.0322H21C21.5523 11.0322 22 11.4799 22 12.0322C22 12.5845 21.5523 13.0322 21 13.0322H3C2.44772 13.0322 2 12.5845 2 12.0322Z"
+              fill="currentColor"
+            />
+            <path
+              d="M3 17.0645C2.44772 17.0645 2 17.5122 2 18.0645C2 18.6167 2.44772 19.0645 3 19.0645H21C21.5523 19.0645 22 18.6167 22 18.0645C22 17.5122 21.5523 17.0645 21 17.0645H3Z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
       </div>
     </header>
   );
