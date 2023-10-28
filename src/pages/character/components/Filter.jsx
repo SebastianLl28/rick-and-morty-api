392b20e1fbdd292439025ecff653deee78036563
SebastianLl28 <llamucasebas@gmail.com>
2023-10-28 12:29:20 -0500

    style (characters): add filter sticky

--- src/pages/character/components/Filter.jsx
+++ src/pages/character/components/Filter.jsx
@@ -5,11 +5,13 @@ import Type from "./Type";
 
 const Filter = () => {
   return (
-    <div className="w-96 space-y-10">
-      <Status />
-      <Gender />
-      <Species />
-      <Type />
+    <div className="w-96">
+      <div className="space-y-10 sticky top-0">
+        <Status />
+        <Gender />
+        <Species />
+        <Type />
+      </div>
     </div>
   );
 };
