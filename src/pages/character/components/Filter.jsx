24f0e26225fa1a6e2789678cd7a008abaca2e04f
SebastianLl28 <llamucasebas@gmail.com>
2023-11-03 16:05:52 -0500

    style(characters): accordion component

--- src/pages/character/components/Filter.jsx
+++ src/pages/character/components/Filter.jsx
@@ -6,7 +6,7 @@ import Type from "./Type";
 const Filter = () => {
   return (
     <div className="w-full xl:w-96 2xl:w-[26rem]">
-      <div className="space-y-10 sticky top-0 sm:grid sm:grid-cols-2 sm:gap-x-10 sm:gap-y-6 sm:space-y-0 md:gap-x-16 xl:block xl:space-y-10">
+      <div className="space-y-10 sticky top-0 sm:grid sm:grid-cols-2 sm:gap-x-10 sm:gap-y-6 sm:space-y-0 md:gap-x-16 xl:block xl:space-y-7">
         <Status />
         <Gender />
         <Species />
--- src/pages/character/components/Status.jsx
+++ src/pages/character/components/Status.jsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { FilterContext } from "../../../context/filterContext";
 
 const Status = () => {
+  const [active, setActive] = useState(true);
   const { initialState, setInitialState } = useContext(FilterContext);
 
   const handleOptionChange = (e) => {
@@ -10,8 +11,31 @@ const Status = () => {
 
   return (
     <div className="flex flex-col">
-      <p className="text-lg font-bold mb-3">Status</p>
-      <div className="space-y-2">
+      <div
+        className="bg-gray-800 p-3 rounded flex justify-between items-center cursor-pointer"
+        onClick={() => setActive(!active)}
+      >
+        <p className="text-lg font-bold select-none">Status</p>
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 24 24"
+          fill="none"
+          className={`transition-transform ${
+            active ? "-rotate-90" : "rotate-0"
+          }`}
+        >
+          <path
+            d="M16.2426 6.34317L14.8284 4.92896L7.75739 12L14.8285 19.0711L16.2427 17.6569L10.5858 12L16.2426 6.34317Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
+      <div
+        className={`space-y-2 overflow-hidden w-[95%] rounded-b mx-auto bg-gray-900 px-4 ${
+          active ? "h-auto py-4" : "h-0"
+        }`}
+      >
         <div className="flex justify-between">
           <label htmlFor="status0">All</label>
           <input
