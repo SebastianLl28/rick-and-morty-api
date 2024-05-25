257365d5e981a460e8d2f401c23b965f2830bc11
Llamuca Dueñas Sebastián <llamucasebas@gmail.com>
2024-05-25 09:42:07 -0500

    refactor: Lazy load pages for improved performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+
+// components
 import Header from "./shared/Header";
-import HomePage from "./pages/home/HomePage";
-import CharacterPage from "./pages/character/CharacterPage";
+
+// context
 import UseFilter from "./context/filterContext";
-import Character from "./pages/character-id/Character";
-import NotFound from "./pages/404/NotFound";
 import Locations from "./pages/locations/Locations";
 import LocationProvider from "./context/locationContext";
+
+// pages
+const HomePage = lazy(() => import("./pages/home/HomePage"));
+const CharacterPage = lazy(() => import("./pages/character/CharacterPage"));
+const Character = lazy(() => import("./pages/character-id/Character"));
+const NotFound = lazy(() => import("./pages/404/NotFound"));
 
 const App = () => {
   return (
@@ -17,13 +24,15 @@ const App = () => {
         <LocationProvider>
           <BrowserRouter>
             <Header />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/characters" element={<CharacterPage />} />
-              <Route path="/characters/:id" element={<Character />} />
-              <Route path="/locations" element={<Locations />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/characters" element={<CharacterPage />} />
+                <Route path="/characters/:id" element={<Character />} />
+                <Route path="/locations" element={<Locations />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </LocationProvider>
       </UseFilter>
