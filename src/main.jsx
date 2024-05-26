d5f8f72dc8b09f02d880318319147726e38c2f48
Llamuca Dueñas Sebastián <llamucasebas@gmail.com>
2024-05-26 18:13:22 -0500

    chore: Add @tanstack/react-query-devtools dependency

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={new QueryClient()}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={new QueryClient()}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <App />
-    </React.StrictMode>
-  </QueryClientProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
